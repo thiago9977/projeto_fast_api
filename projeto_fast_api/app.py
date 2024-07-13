9d1b0051c79b5cb0f2331c8496e8b98b8b1051af
Thiago9977 <lakavana265@gmail.com>
2024-07-12 23:20:31 -0300

    commitado com sucesso

--- projeto_fast_api/app.py
+++ projeto_fast_api/app.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from http import HTTPStatus
+
+from projeto_fast_api.schemas import Message
+
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code= HTTPStatus.OK, response_model= Message)
 def root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'italo é o juliusip'}
